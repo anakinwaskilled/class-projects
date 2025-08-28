7826a5b798b4f5425855dd3b18366029f9897acb
anakinwaskilled <akshatsaini781@gmail.com>
2025-08-29 01:23:26 +0530

    update

--- himanshu.cpp
+++ himanshu.cpp
@@ -1,2 +1,56 @@
-lrvbcejl.wen ,jdf
-cle
+#include <iostream>
+using namespace std;
+
+class b
+{
+int a;
+public:
+int b;
+void get_ab();
+int get_a();
+void show_a();
+};
+
+class d : public b
+{
+int c;
+public: 
+void mul();
+void display();
+};
+
+void b::get_ab()
+{
+a=10;
+b=20;
+cin>>a>>b;
+
+}
+int b::get_a()
+{
+return a;
+}
+void b::show_a()
+{
+cout<<"a="<<a<<endl;
+}
+
+void d::mul()
+{
+c=get_a()*b;
+}
+void d::display()
+{
+cout<<"a="<<get_a()<<"/n";
+cout<<"b="<<b<<endl;
+cout<<"c="<<c<<"/n";
+}
+
+
+int main()
+{
+d obj;
+obj.get_ab();
+obj.mul();
+obj.display();
+}
